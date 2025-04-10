ee25f8e32779b2bcc504eb700c70fab456d4c9c5
Jon Tran <jonathant5678@gmail.com>
2025-04-10 12:32:45 -0400

    cuda llama

--- cagfull.py
+++ cagfull.py
@@ -47,17 +47,17 @@ def clean_up(cache: DynamicCache, origin_len: int):
         cache.key_cache[i] = cache.key_cache[i][:, :, :origin_len, :]
         cache.value_cache[i] = cache.value_cache[i][:, :, :origin_len, :]
 
-#def get_device_map() -> str:
-#    return 'cuda' if torch.cuda.is_available() else 'cpu'
+def get_device_map() -> str:
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
 
-#device = get_device_map() #cpu
+device = get_device_map() #cpu
 
 model_name = "meta-llama/Llama-3.2-1B"#"mistralai/Mistral-7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_name, token="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", trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
-    device_map="cpu",#device,
+    device_map=device,
     trust_remote_code=True,
     token="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"
 )
